f225825d1c60fd30f980e7eebae3e6aa5d6eb954
A.I.Hunter <Walkercpt@gmail.com>
2025-05-08 11:34:27 +0800

    Enhance MerchantKale dialogue logic based on player inventory and surroundings
    - Updated MerchantKale sayDialogue method such that it will dynamically return certain dialogues based on pre-determined conditions

--- src/game/actors/npcs/MerchantKale.java
+++ src/game/actors/npcs/MerchantKale.java
@@ -2,8 +2,13 @@ package game.actors.npcs;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.grounds.GroundStatus;
+
+import java.util.List;
 
 public class MerchantKale extends NPC {
     /**
@@ -20,5 +25,32 @@ public class MerchantKale extends NPC {
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         return null;
+    }
+
+    @Override
+    public String sayDialogue(Actor actor, GameMap map) {
+        boolean nearCursedGround = false;
+        List<Exit> exits = map.locationOf(this).getExits();
+
+        for (Exit exit : exits) {
+            if (exit.getDestination().getGround().hasCapability(GroundStatus.CURSED)) {
+                nearCursedGround = true;
+                break;
+            }
+        }
+
+        try {
+            if (actor.getItemInventory().isEmpty()) {
+                return this.getDialogues().get("empty");
+            } else if (actor.getBalance() < 500) {
+                return this.getDialogues().get("less");
+            } else if (nearCursedGround) {
+                return this.getDialogues().get("cursed");
+            } else {
+                return this.getDialogues().get("default");
+            }
+        } catch (IndexOutOfBoundsException e) {
+            return "...(silence)";
+        }
     }
 }
