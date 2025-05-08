c558e51947c30d2cc0d8c5e385cf680c61b93b87
A.I.Hunter <Walkercpt@gmail.com>
2025-05-08 11:35:35 +0800

    Implement dynamic dialogue responses based on actor health in Guts class
    - Updated Guts sayDialogue method such that it will return the correct dialogue if certain conditions are met otherwise it will show other dialogue options

--- src/game/actors/npcs/Guts.java
+++ src/game/actors/npcs/Guts.java
@@ -2,6 +2,8 @@ package game.actors.npcs;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 
@@ -19,5 +21,23 @@ public class Guts extends NPC {
     @Override
     public Action playTurn(ActionList actions, Action lastAction, GameMap map, Display display) {
         return null;
+    }
+
+    @Override
+    public String sayDialogue(Actor actor, GameMap map) {
+        int size = this.getDialogues().size();
+
+        try {
+
+            if (actor.getAttribute(BaseActorAttributes.HEALTH) < 50) {
+                return this.getDialogues().get("weak");
+            } else {
+                String randKey = String.valueOf(this.getRandom().nextInt(size));
+                return this.getDialogues().get(randKey);
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+            return "...(silence)";
+        }
     }
 }
