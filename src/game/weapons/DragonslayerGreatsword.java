f8acb38883e8309a1a214b2d915c38a24b9e1aeb
malm0022 <malm0022@student.monash.edu>
2025-05-09 20:45:29 +0800

    Added DragonslayerGreatsword Class
    - initial commit for a new kind of weapon in the game

---
+++ src/game/weapons/DragonslayerGreatsword.java
@@ -0,0 +1,11 @@
+package game.weapons;
+
+public class DragonslayerGreatsword extends WeaponItem {
+
+    public DragonslayerGreatsword() {
+
+        super("Dragonslayer Greatsword", 'D', 70, "strikes", 75);
+
+    }
+
+}
