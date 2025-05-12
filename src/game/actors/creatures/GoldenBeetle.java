fc049108736270232a3ff105bb6f1413a4c7a501
anfal <aahs0004@student.monash.edu>
2025-05-12 22:23:35 +0800

    Modified GoldenBeetle class to implement Edible - implemented eat() to show the consumption of the beetle -implemented allowableActions() to allow the farmer to eat beetle

--- src/game/actors/creatures/GoldenBeetle.java
+++ src/game/actors/creatures/GoldenBeetle.java
@@ -1,9 +1,19 @@
 package game.actors.creatures;
 
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
 import edu.monash.fit2099.engine.actors.attributes.BaseActorAttribute;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
+import game.actions.CureAction;
+import game.actions.EatAction;
+import game.actors.Ability;
 import game.behaviours.WanderBehaviour;
+import game.items.eggs.Edible;
+import game.items.eggs.GoldenEgg;
+import game.items.eggs.OmenSheepEgg;
 
 public class GoldenBeetle extends Creature implements Reproductive, Edible {
 
@@ -29,5 +39,21 @@ public class GoldenBeetle extends Creature implements Reproductive, Edible {
             location.addItem(new GoldenEgg());
             eggLayCounter = 0;
         }
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+        ActionList actionList = super.allowableActions(otherActor, direction, map);
+        actionList.add(new EatAction(this));
+
+        return actionList;
+    }
+
+    @Override
+    public String eat(Actor actor, GameMap map) {
+        actor.modifyAttributeMaximum(BaseActorAttributes.HEALTH, ActorAttributeOperations.INCREASE, 15);
+        actor.addBalance(1000);
+        map.removeActor(this);
+        return actor + " eats the beetle.";
     }
 }
