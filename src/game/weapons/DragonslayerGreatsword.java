652844d66ff6e021b60a4feee18f87c23597c4ca
malm0022 <malm0022@student.monash.edu>
2025-05-12 17:01:30 +0800

    Update DragonslayerGreatsword class
    - implemented Buyable interface
    
    - added cost
    
    - implemented onPurchase to increase maximum health of the player.

--- src/game/weapons/DragonslayerGreatsword.java
+++ src/game/weapons/DragonslayerGreatsword.java
@@ -1,11 +1,29 @@
 package game.weapons;
 
-public class DragonslayerGreatsword extends WeaponItem {
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 
-    public DragonslayerGreatsword() {
+public class DragonslayerGreatsword extends WeaponItem implements Buyable {
+
+    private int cost;
+
+    public DragonslayerGreatsword(int cost) {
 
         super("Dragonslayer Greatsword", 'D', 70, "strikes", 75);
+        this.cost = cost;
 
     }
 
+    @Override
+    public int getCost() {
+        return cost;
+    }
+
+    @Override
+    public void onPurchase(Actor buyer) {
+
+        buyer.modifyAttributeMaximum(BaseActorAttributes.HEALTH, ActorAttributeOperations.INCREASE, 15);
+
+    }
 }
