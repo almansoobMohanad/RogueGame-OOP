d9441358cdda61f6ef3000c242275670781defa4
malm0022 <malm0022@student.monash.edu>
2025-05-12 17:04:03 +0800

    Update Sellen class
    - implemented Merchant interface so they can return Sell actions and decide what effect happens when selling.
    
    - added sellItems (a list of item Sellen can sell)
    
    - added the weapons sellen can sell

--- src/game/actors/npcs/Sellen.java
+++ src/game/actors/npcs/Sellen.java
@@ -3,12 +3,22 @@ package game.actors.npcs;
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
+import game.actions.BuyAction;
+import game.actors.Merchant;
+import game.weapons.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Sellen extends NPC {
+public class Sellen extends NPC implements Merchant {
+
+    private final List<Buyable> sellItems = new ArrayList<>();
+
     /**
      * Constructor for Sellen
      */
@@ -16,6 +26,11 @@ public class Sellen extends NPC {
         super("Sellen", 's', 150);
         this.addDialogue("0","The academy casts out those it fears. Yet knowledge, like the stars, cannot be bound forever.");
         this.addDialogue("1","You sense it too, don’t you? The Glintstone hums, even now.");
+
+        sellItems.add(new BroadSword(100));
+        sellItems.add(new DragonslayerGreatsword(1500));
+        sellItems.add(new Katana(500));
+
     }
 
     @Override
@@ -34,5 +49,52 @@ public class Sellen extends NPC {
         } catch (IndexOutOfBoundsException e) {
             return "...(silence)";
         }
+    }
+
+    @Override
+    public ActionList getSellActions() {
+
+        ActionList actionList = new ActionList();
+
+        for (Buyable item: sellItems) {
+
+            actionList.add(new BuyAction(item, this));
+
+        }
+
+        return actionList;
+
+    }
+
+    @Override
+    public void decideEffect(Buyable item, Actor buyer) {
+
+        if (item instanceof BroadSword) {
+
+            int previousMaxHealth = buyer.getAttributeMaximum(BaseActorAttributes.HEALTH);
+
+            buyer.modifyAttributeMaximum(BaseActorAttributes.HEALTH, ActorAttributeOperations.UPDATE, previousMaxHealth + 20);
+            
+        } else if (item instanceof DragonslayerGreatsword) {
+
+            System.out.println("GOLDEN BEETLE SHOULD SPAWN");
+            
+        } else if (item instanceof Katana) {
+
+            buyer.heal(10);
+            System.out.println("OMEN SHEEP SHOULD SPAWN");
+            
+        }
+        
+    }
+
+    @Override
+    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+
+        ActionList actionList = new ActionList();
+
+        actionList.add(getSellActions());
+
+        return actionList;
     }
 }
