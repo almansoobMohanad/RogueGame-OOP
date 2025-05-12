994901173fe21dc5d16d8d2b31312fa197c20907
malm0022 <malm0022@student.monash.edu>
2025-05-12 17:00:27 +0800

    Update BroadSword
    - implemented Buyable interface
    
    - added cost parameter
    
    - added onPurchase effect when bought.

--- src/game/weapons/BroadSword.java
+++ src/game/weapons/BroadSword.java
@@ -1,13 +1,27 @@
 package game.weapons;
 
-public class BroadSword extends WeaponItem {
+import edu.monash.fit2099.engine.actors.Actor;
 
-    public BroadSword() {
+public class BroadSword extends WeaponItem implements Buyable {
+
+    private int cost;
+
+    public BroadSword(int cost) {
 
         super("Broadsword", 'b', 30, "slashes", 50);
+        this.cost = cost;
 
     }
 
+    @Override
+    public int getCost() {
+        return cost;
+    }
 
+    @Override
+    public void onPurchase(Actor buyer) {
 
+        buyer.heal(10);
+
+    }
 }
