777682dc1eab5bf5d0fca0c38ab01e8c7bc691ae
anfal <aahs0004@student.monash.edu>
2025-05-12 22:21:38 +0800

    Modified GoldenBeetle class to implement Reproductive - implemented the reproduce method to add an egg every five turns

--- src/game/actors/creatures/GoldenBeetle.java
+++ src/game/actors/creatures/GoldenBeetle.java
@@ -5,7 +5,9 @@ import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
 import game.behaviours.WanderBehaviour;
 
-public class GoldenBeetle extends Creature implements Reproductive {
+public class GoldenBeetle extends Creature implements Reproductive, Edible {
+
+    private int eggLayCounter = 0;
 
 
     public GoldenBeetle() {
@@ -13,8 +15,19 @@ public class GoldenBeetle extends Creature implements Reproductive {
         addBehaviour(0, new WanderBehaviour());
     }
 
+    /**
+     * Lays a new Golden Beetle Egg when the counter reaches threshold.
+     *
+     * @param map the game map of Omen Sheep
+     * @param location the location of Omen Sheep on the map
+     */
     @Override
-    public void reproduce(GameMap map, Location location) {
+    public void reproduce(GameMap map,Location location) {
+        eggLayCounter++;
 
+        if (eggLayCounter >= 5) {
+            location.addItem(new GoldenEgg());
+            eggLayCounter = 0;
+        }
     }
 }
