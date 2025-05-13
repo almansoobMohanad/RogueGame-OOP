52adc6f5db94614e1a61260c82887609342d1979
malm0022 <malm0022@student.monash.edu>
2025-05-14 00:49:18 +0800

    Update DragonslayerGreatsword
    - Added EffectList attribute for all of the effects for buying this item.
    
    - implemented effectlist.apply() to apply all side effects of purchasing this item.

--- src/game/weapons/DragonslayerGreatsword.java
+++ src/game/weapons/DragonslayerGreatsword.java
@@ -3,15 +3,20 @@ package game.weapons;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
 import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.Merchant;
+import game.effects.EffectsList;
 
 public class DragonslayerGreatsword extends WeaponItem implements Buyable {
 
     private int cost;
+    private EffectsList effectsList;
 
-    public DragonslayerGreatsword(int cost) {
+    public DragonslayerGreatsword(int cost, EffectsList effectsList) {
 
         super("Dragonslayer Greatsword", 'D', 70, "strikes", 75);
         this.cost = cost;
+        this.effectsList = effectsList;
 
     }
 
@@ -21,9 +26,10 @@ public class DragonslayerGreatsword extends WeaponItem implements Buyable {
     }
 
     @Override
-    public void onPurchase(Actor buyer) {
+    public void onPurchase(Actor buyer, Merchant merchant, GameMap gameMap) {
 
         buyer.modifyAttributeMaximum(BaseActorAttributes.HEALTH, ActorAttributeOperations.INCREASE, 15);
+        effectsList.apply(buyer, gameMap);
 
     }
 }
