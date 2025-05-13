93d3745d2b68021fc031e64a96743dc6a7e989b8
malm0022 <malm0022@student.monash.edu>
2025-05-14 00:48:58 +0800

    Update BroadSword
    - Added EffectList attribute for all of the effects for buying this item.
    
    - implemented effectlist.apply() to apply all side effects of purchasing this item.

--- src/game/weapons/BroadSword.java
+++ src/game/weapons/BroadSword.java
@@ -1,15 +1,20 @@
 package game.weapons;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.Merchant;
+import game.effects.EffectsList;
 
 public class BroadSword extends WeaponItem implements Buyable {
 
     private int cost;
+    private EffectsList effectsList;
 
-    public BroadSword(int cost) {
+    public BroadSword(int cost, EffectsList effectsList) {
 
         super("Broadsword", 'b', 30, "slashes", 50);
         this.cost = cost;
+        this.effectsList = effectsList;
 
     }
 
@@ -19,9 +24,10 @@ public class BroadSword extends WeaponItem implements Buyable {
     }
 
     @Override
-    public void onPurchase(Actor buyer) {
+    public void onPurchase(Actor buyer, Merchant merchant, GameMap gameMap) {
 
         buyer.heal(10);
+        effectsList.apply(buyer, gameMap);
 
     }
 }
