34506c27ed8b4c72c0e0d62bc30fdc57bd18be42
malm0022 <malm0022@student.monash.edu>
2025-05-14 00:45:25 +0800

    Update Buyable Interface
    - Onpurchase now requires Actor, Merchant and GameMap as an argument

--- src/game/weapons/Buyable.java
+++ src/game/weapons/Buyable.java
@@ -1,10 +1,12 @@
 package game.weapons;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.Merchant;
 
 public interface Buyable {
 
     int getCost();
-    void onPurchase(Actor buyer);
+    void onPurchase(Actor buyer, Merchant merchant, GameMap gameMap);
 
 }
