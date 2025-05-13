ae4a6ace961757cc42b613bb440cfeb8e837a446
malm0022 <malm0022@student.monash.edu>
2025-05-14 00:49:26 +0800

    Update Katana
    - Added EffectList attribute for all of the effects for buying this item.
    
    - implemented effectlist.apply() to apply all side effects of purchasing this item.

--- src/game/weapons/Katana.java
+++ src/game/weapons/Katana.java
@@ -1,15 +1,20 @@
 package game.weapons;
 
 import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import game.actors.Merchant;
+import game.effects.EffectsList;
 
 public class Katana extends WeaponItem implements Buyable {
 
     private int cost;
+    private EffectsList effectsList;
 
-    public Katana(int cost) {
+    public Katana(int cost, EffectsList effectsList) {
 
         super("Katana", 'j', 50, "delivers", 60);
         this.cost = cost;
+        this.effectsList = effectsList;
 
     }
 
@@ -19,7 +24,8 @@ public class Katana extends WeaponItem implements Buyable {
     }
 
     @Override
-    public void onPurchase(Actor buyer) {
+    public void onPurchase(Actor buyer, Merchant merchant, GameMap gameMap) {
         buyer.hurt(25);
+        effectsList.apply(buyer, gameMap);
     }
 }
