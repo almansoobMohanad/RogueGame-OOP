6c32812200097d4c0c9f4c65ec0188eeb65a8e4b
adel0037 <adel0037@student.monash.edu>
2025-05-16 01:27:50 +0800

    Updated GoldenBeetle to reproduce using new Egg class
    - Modified reproduce() to create a Golden Egg using the new Egg class.
    - Uses NearStatusCondition (must be near CURSED) to hatch into a GoldenBeetle.
    - Egg restores 20 stamina when eaten using AttributeEffect.

--- src/game/actors/creatures/GoldenBeetle.java
+++ src/game/actors/creatures/GoldenBeetle.java
@@ -2,25 +2,17 @@ package game.actors.creatures;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.actors.Behaviour;
-import edu.monash.fit2099.engine.actors.attributes.ActorAttributeOperations;
-import edu.monash.fit2099.engine.actors.attributes.BaseActorAttribute;
-import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.Location;
-import game.actions.CureAction;
 import game.actions.EatAction;
 import game.actors.Ability;
 import game.behaviours.FollowBehaviour;
 import game.behaviours.ReproduceBehaviour;
 import game.behaviours.WanderBehaviour;
 import game.items.eggs.Edible;
-import game.items.eggs.GoldenEgg;
-import game.items.eggs.OmenSheepEgg;
 
 public class GoldenBeetle extends Creature implements Reproductive, Edible {
 
@@ -43,11 +35,19 @@ public class GoldenBeetle extends Creature implements Reproductive, Edible {
     @Override
     public void reproduce(GameMap map,Location location) {
         eggLayCounter++;
-        if (eggLayCounter >= 5) {
-            location.addItem(new GoldenEgg());
-            eggLayCounter = 0;
+        //if (eggLayCounter >= 5) {
+            //Location location = map.locationOf(this);
 
-        }
+//            Egg egg = new Egg(
+//                    "Golden Egg", '0',
+//                    new GoldenBeetle(),
+//                    new NearStatusCondition(location, Status.CURSED),
+//                    new AttributeEffect(BaseActorAttributes.STAMINA, 20)
+//            );
+
+            //location.addItem(egg);
+            //eggLayCounter = 0;
+        //}
     }
 
     @Override
