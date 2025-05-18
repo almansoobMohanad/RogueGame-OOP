13c34412b7f7d762d886a1d1fa906a65018f7f86
malm0022 <malm0022@student.monash.edu>
2025-05-18 23:46:43 +0800

    Added JavaDocs
    - added javaDocs for many classes (Broadsword, Buyable, BuyAction, DragonslayerGreatsword, Katana, ListenAction, Merchant)

--- src/game/actions/ListenAction.java
+++ src/game/actions/ListenAction.java
@@ -7,22 +7,61 @@ import game.actors.npcs.NPC;
 
 import java.util.Random;
 
+/**
+ * An action that allows an {@link Actor} to listen to a monologue from an {@link NPC}.
+ * <p>
+ * When executed, this action retrieves a random line from the NPC's monologue and returns it.
+ * </p>
+ *
+ * Example usage:
+ * <pre>
+ *     new ListenAction(fingerReaderEnia);
+ * </pre>
+ *
+ * This adds immersion and character interaction to the game.
+ *
+ * @author Adji
+ */
 public class ListenAction extends Action {
 
+    /**
+     * The NPC from whom the actor will listen.
+     */
     private final NPC npc;
+
+    /**
+     * Random number generator to choose a random monologue line.
+     */
     private final Random random;
 
+    /**
+     * Constructs a new {@code ListenAction}.
+     *
+     * @param npc the NPC to listen to
+     */
     public ListenAction(NPC npc) {
         this.npc = npc;
         this.random = new Random();
     }
 
+    /**
+     * Executes the action, allowing the actor to hear a random line from the NPC's monologue.
+     *
+     * @param actor the actor performing the action
+     * @param map   the current game map
+     * @return a string containing the randomly selected monologue line
+     */
     @Override
     public String execute(Actor actor, GameMap map) {
         return this.npc.sayMonologue(actor, map).get(random.nextInt(this.npc.sayMonologue(actor, map).size())).listen();
     }
 
-
+    /**
+     * Provides a description of this action suitable for displaying in a menu.
+     *
+     * @param actor the actor performing the action
+     * @return a string describing the listen action
+     */
     @Override
     public String menuDescription(Actor actor) {
         return actor + " listens to " + this.npc;
