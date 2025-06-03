014b4883738fb88ff2475f0509fc75a537e66c1f
ahak0006 <ahak0006@student.monash.edu>
2025-06-03 19:48:22 +0800

    Add EldenThingGameMap class to integrate time tracking functionality

---
+++ src/game/EldenThingGameMap.java
@@ -0,0 +1,22 @@
+package game;
+
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.GroundFactory;
+import game.timemanagement.TimeTracker;
+
+import java.util.List;
+
+public class EldenThingGameMap extends GameMap {
+    private final TimeTracker timeTracker;
+    public EldenThingGameMap(String name, GroundFactory groundFactory, List<String> lines, TimeTracker timeTracker) {
+        super(name, groundFactory, lines);
+        this.timeTracker = timeTracker;
+    }
+
+    public void tick() {
+        if (timeTracker != null) {
+            timeTracker.tick();
+        }
+        super.tick();
+    }
+}
