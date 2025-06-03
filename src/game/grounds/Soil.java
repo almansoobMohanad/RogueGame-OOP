e8eced6ee7219b61b1965085393eec94cc4da271
ahak0006 <ahak0006@student.monash.edu>
2025-06-04 01:33:37 +0800

    Updated NightingaleBerry and Soil to implement the features and made Soil TimeAware; Added MultiplierEffect to increase the multiplier of an actor
    - Updated NightingaleBerry to implement all of its features and made it Edible
    - Updated Soil so that it has 25% chance of spawning NightingaleBerry
    - Added MultiplierEffect to increase the multiplier of the Actor

--- src/game/grounds/Soil.java
+++ src/game/grounds/Soil.java
@@ -1,14 +1,43 @@
 package game.grounds;
 
 import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.items.NightingaleBerry;
+import game.timemanagement.Phases;
+import game.timemanagement.TimeAware;
+
+import java.util.Random;
 
 /**
  * A class representing the soil in the valley
  * @author Adrian Kristanto
+ *
+ * Modified by Adji Ilhamhafiz Sarie Hakim
  */
-public class Soil extends Ground {
+public class Soil extends Ground implements TimeAware {
+    private Phases currentPhase;
+    private final Random random;
+    private static final int NIGHTINGALE_BERRY_SPAWN_CHANCE = 25;
+
+
     public Soil() {
         super('.', "Soil");
         this.addCapability(GroundStatus.PLANTABLE);
+        this.random = new Random();
+    }
+
+    @Override
+    public void tick(Location location) {
+        super.tick(location);
+        if (this.currentPhase == Phases.NIGHT){
+            if (random.nextInt(100) < NIGHTINGALE_BERRY_SPAWN_CHANCE) {
+                location.addItem(new NightingaleBerry());
+            }
+        }
+    }
+
+    @Override
+    public void onTimeChange(Phases newPhase) {
+        currentPhase = newPhase;
     }
 }
--- src/game/items/NightingaleBerry.java
+++ src/game/items/NightingaleBerry.java
@@ -1,22 +1,65 @@
 package game.items;
 
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.actors.attributes.BaseActorAttributes;
 import edu.monash.fit2099.engine.items.Item;
-import game.timemanagement.Phase;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.actions.EatAction;
+import game.effects.AttributeEffect;
+import game.effects.EffectsList;
+import game.effects.MultiplierEffect;
+import game.items.eggs.Edible;
+import game.timemanagement.Phases;
 import game.timemanagement.TimeAware;
 
-public class NightingaleBerry extends Item implements TimeAware {
-    /***
+public class NightingaleBerry extends Item implements TimeAware, Edible {
+
+    private Phases currentPhase;
+    private final EffectsList effectsList;
+    private static final int HEALTH_INCREASE_AMOUNT = 20;
+    private static final float MULTIPLIER_UPDATED_VALUE = 1.5f;
+    /**
      * Constructor.
-     *  @param name the name of this Item
-     * @param displayChar the character to use to represent this item if it is on the ground
-     * @param portable true if and only if the Item can be picked up
      */
-    public NightingaleBerry(String name, char displayChar, boolean portable) {
-        super(name, displayChar, portable);
+    public NightingaleBerry() {
+        super("Nightingale Berry", '^', true);
+        this.effectsList = new EffectsList();
+        this.saturateEffectsList();
+    }
+
+    private void saturateEffectsList(){
+        this.effectsList.addEffect(new AttributeEffect(BaseActorAttributes.HEALTH, HEALTH_INCREASE_AMOUNT));
+        this.effectsList.addEffect(new MultiplierEffect(MULTIPLIER_UPDATED_VALUE));
     }
 
     @Override
-    public void onTimeChange(Phase newPhase) {
+    public void onTimeChange(Phases newPhase) {
+        this.currentPhase = newPhase;
+    }
 
+    @Override
+    public void tick(Location currentLocation) {
+        super.tick(currentLocation);
+        if (currentPhase == Phases.DAY){
+            currentLocation.removeItem(this);
+        }
+    }
+
+    @Override
+    public String eat(Actor actor, GameMap map) {
+        effectsList.apply(actor, map);
+        actor.removeItemFromInventory(this);
+        return actor + " eats the Nightingale Berry.";
+    }
+
+    @Override
+    public ActionList allowableActions(Actor owner, GameMap map){
+        ActionList actions = super.allowableActions(owner, map);
+
+        actions.add(new EatAction(this));
+
+        return actions;
     }
 }
