6d07af3e1b05eb22ae0c6739b31bc8636ec41df3
ahak0006 <ahak0006@student.monash.edu>
2025-06-06 00:43:11 +0800

    Implement TimeAware interface in EldenThingGameMap; enhance tick method to spawn SuspiciousMerchant based on time phases
    - Updated EldenThingGameMap such that the Application will be able to implement the time tracking and spawning properly

--- src/game/EldenThingGameMap.java
+++ src/game/EldenThingGameMap.java
@@ -2,28 +2,69 @@ package game;
 
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.positions.GroundFactory;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.positions.NumberRange;
 import game.actors.npcs.SuspiciousMerchant;
 import game.behaviours.StandardNPCController;
-import game.timemanagement.Phases;
-import game.timemanagement.TimeTracker;
+import game.timemanagement.*;
 
 import java.util.List;
+import java.util.Random;
 
-public class EldenThingGameMap extends GameMap {
+public class EldenThingGameMap extends GameMap implements TimeAware {
     private final TimeTracker timeTracker;
-    public EldenThingGameMap(String name, GroundFactory groundFactory, List<String> lines, TimeTracker timeTracker) {
+    private final TimeProvider timeProvider;
+    private Random random;
+    boolean merchantSpawned;
+
+    public EldenThingGameMap(String name, GroundFactory groundFactory, List<String> lines) {
         super(name, groundFactory, lines);
+        this.timeTracker = null;
+        this.timeProvider = ServiceLocator.getTimeProvider();
+        this.random = new Random();
+        this.merchantSpawned = false;
+    }
+
+    public EldenThingGameMap(String name, GroundFactory groundFactory, char groundChar, int width, int height, TimeTracker timeTracker) {
+        super(name, groundFactory, groundChar, width, height);
         this.timeTracker = timeTracker;
+        this.timeProvider = null;
     }
 
     public void tick() {
         if (timeTracker != null) {
             timeTracker.tick();
+        }
 
-            if (timeTracker.getCurrentPhase() == Phases.NIGHT) {
-                this.at(20, 14).addActor(new SuspiciousMerchant(new StandardNPCController()));
+        if (random != null) {
+            NumberRange xRange = this.getXRange();
+            NumberRange yRange = this.getYRange();
+
+            int randX = random.nextInt(xRange.min(), xRange.max());
+            int randY = random.nextInt(yRange.min(), yRange.max());
+
+            this.onTimeChange(this.at(randX, randY));
+        }
+
+        super.tick();
+    }
+
+    @Override
+    public void onTimeChange(Location location) {
+        if (timeProvider != null) {
+            Phases currentPhase = this.timeProvider.getCurrentPhase();
+            if (currentPhase == Phases.NIGHT && !merchantSpawned) {
+
+                boolean containsActor = location.containsAnActor();
+
+                if (!containsActor) {
+                    location.addActor(new SuspiciousMerchant(new StandardNPCController()));
+                }
+
+                merchantSpawned = true;
+            } else if (currentPhase == Phases.DAY) {
+                merchantSpawned = false;
             }
         }
-        super.tick();
     }
 }
