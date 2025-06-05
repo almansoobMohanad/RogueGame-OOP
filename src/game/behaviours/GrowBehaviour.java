839ae8de4f10f7379411481bfc2dec1a07035e71
adel0037 <adel0037@student.monash.edu>
2025-06-06 00:18:03 +0800

    Modified GrowBehaviour to return GrowAction

--- src/game/behaviours/GrowBehaviour.java
+++ src/game/behaviours/GrowBehaviour.java
@@ -1,31 +1,17 @@
 package game.behaviours;
 
 import edu.monash.fit2099.engine.actions.Action;
-import edu.monash.fit2099.engine.actions.DoNothingAction;
 import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.actors.Behaviour;
-import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
-import game.actors.creatures.BedOfChaos;
-import game.actors.creatures.Branch;
-
-import java.util.List;
+import game.actors.creatures.boss.BedOfChaos;
+import game.actors.creatures.boss.GrowAction;
 
 public class GrowBehaviour implements Behaviour {
 
     @Override
     public Action getAction(Actor actor, GameMap map) {
         BedOfChaos boss = (BedOfChaos) actor;
-        Display display = new Display();
-
-        List<Branch> oldBranches = boss.getBranches();
-
-        boss.growChain(display);
-
-        for (Branch b : oldBranches) {
-            b.chainGrow(display);
-        }
-
-        return new DoNothingAction();
+        return new GrowAction(boss);
     }
 }
