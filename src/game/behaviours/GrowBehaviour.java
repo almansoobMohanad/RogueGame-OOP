71aabed474a13bc29b2e72437d67fd9cdab8a9d5
adel0037 <adel0037@student.monash.edu>
2025-06-05 16:07:40 +0800

    Fixed boss growth logic in GrowBehaviour
    - Loop through existing branches to call chainGrow() so new branches do not grow in the same turn.

--- src/game/behaviours/GrowBehaviour.java
+++ src/game/behaviours/GrowBehaviour.java
@@ -7,20 +7,23 @@ import edu.monash.fit2099.engine.actors.Behaviour;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import game.actors.creatures.BedOfChaos;
+import game.actors.creatures.Branch;
+
+import java.util.List;
 
 public class GrowBehaviour implements Behaviour {
 
     @Override
     public Action getAction(Actor actor, GameMap map) {
         BedOfChaos boss = (BedOfChaos) actor;
-
         Display display = new Display();
 
-        if (boss.getBranchCount() == 0) {
-            boss.growChain(display);
-        }
-        else {
-            boss.continueGrowChain(display);
+        List<Branch> oldBranches = boss.getBranches();
+
+        boss.growChain(display);
+
+        for (Branch b : oldBranches) {
+            b.chainGrow(display);
         }
 
         return new DoNothingAction();
