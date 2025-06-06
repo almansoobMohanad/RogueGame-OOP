0cef388e4275b64ba844691bf4c759c4621c2f76
anfal <aahs0004@student.monash.edu>
2025-06-06 23:12:53 +0800

    Added the javadoc to TeleportDestination and TelportCircle classes

--- src/game/grounds/TeleportationCircle.java
+++ src/game/grounds/TeleportationCircle.java
@@ -11,26 +11,54 @@ import game.actors.Status;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A special type of ground that allows actors with the TELEPORT ability
+ * to teleport to predefined destinations.
+ *
+ * Only actors standing on this tile (not adjacent) can teleport.
+ *
+ * Represented by 'A' on the map.
+ * @author anfal
+ */
 public class TeleportationCircle extends Ground {
+    /** List of possible teleport destinations from this circle */
     private final List<TeleportDestination> destinations = new ArrayList<>();
 
-
+    /**
+     * Constructor for the TeleportationCircle.
+     * Initializes with display character 'A' and name "Teleportation Circle".
+     */
     public TeleportationCircle() {
         super('A', "Teleportation Circle");
-        //maybe add a teleport enum
+
     }
 
+    /**
+     * Adds a teleportation destination to the list.
+     *
+     * @param destination the teleportation destination to add
+     */
     public void addDestination(TeleportDestination destination) {
         destinations.add(destination);
     }
 
 
+    /**
+     * Determines what actions an actor can perform at this location.
+     * Only actors currently standing on this tile and possessing the TELEPORT ability
+     * will be given teleport actions.
+     *
+     * @param actor     the Actor acting
+     * @param location  the current Location of the actor
+     * @param direction the direction of the location from the Actor
+     * @return a list of allowable actions (may include teleport actions)
+     */
     @Override
     public ActionList allowableActions(Actor actor, Location location, String direction) {
         ActionList actions = new ActionList();
 
         // Only provide teleport actions if the actor is STANDING on this tile (not adjacent)
-        if (location.getActor() == actor && actor.hasCapability(Ability.TELEPORT)) { //could also add enum for teleportable ground but ig this reduces depedencies
+        if (location.getActor() == actor && actor.hasCapability(Ability.TELEPORT)) {
             for (TeleportDestination dest : destinations) {
                 actions.add(new TeleportAction(dest.getMap(), dest.getLocation()));
             }
